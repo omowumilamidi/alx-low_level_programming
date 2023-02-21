19edd365b5f7637ce3ee57b3b9ee07c2b268bfbe
omowumilamidi <habydemy7@gmail.com>
2023-02-21 06:22:38 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio. h>
+
+/**
+ * main: a programme that print a line with puts function
+ * return 0 ; is a success
+ */
+
+int main(void)
+{
+puts( /"Programming is like building a multilingual puzzle\n");
+return 0 ; 
+}
+
+
