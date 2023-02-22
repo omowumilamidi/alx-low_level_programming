458e157f6e9eebcd2e07611b8cd99dcd6efe2222
omowumilamidi <habydemy7@gmail.com>
2023-02-22 14:51:59 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_times_table - functions that prints times table
+ * @n: the number
+ * Return: if greater than 15 or less than 0
+ */
+void print_times_table(int n)
+{
+	for (n = 0; n <= 0; n--)
+	{
+	_putchar("%d\n", n);
+	}
+
+	for (n = 15; n >= 15; n++)
+		{
+		_putchar("%d\n", n);
+		}
+
+	else
+		(n = 15 && n >= 15) || (n = 0 && n <= 0)
+	{
+		_putchar("%d|n, n);
+
+		return (0);
+	}
+}
