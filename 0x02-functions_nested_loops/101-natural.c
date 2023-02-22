5f82c463d7963c5eeacf12c516ed4287e24339f9
omowumilamidi <habydemy7@gmail.com>
2023-02-22 15:05:07 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - compute and print sum of all multiple numbers
+ * @a: the first number
+ * @b: teh second number
+ * Result: always 0 success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for 
+
