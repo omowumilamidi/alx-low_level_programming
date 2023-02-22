8662e7bb1ef676b76ab1afa7da7a4c0dabef7d3c
omowumilamidi <habydemy7@gmail.com>
2023-02-22 07:56:50 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,10 @@
  */
 void print_alphabet_x10(void)
 {
-	int 10;
+	int ten;
 	char la;
 
-	for (10 = 0; 10 <= 9; 10++)
+	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
