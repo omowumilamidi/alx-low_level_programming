67d8e78d1c541e64a3c2b28c1ff2b968aa7ee2a6
omowumilamidi <habydemy7@gmail.com>
2023-02-22 10:04:16 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add to two integers
+ * @a: the first number
+ * @b: the secomd number
+ * Return: the addition of bot
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
