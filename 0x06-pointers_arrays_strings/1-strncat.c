b72ae53812adcb5269125653cd488d2d42f2e6e3
omowumilamidi <habydemy7@gmail.com>
2023-03-18 07:14:19 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,8 +6,8 @@
 * @n: number of char n to be copied
 * Return: pointer to dest
 */
-*char *_strncat(char *dest, char *src, int n);
-{	
+char *_strncat(char *dest, char *src, int n)
+{
 int i;
 int j;
  	
